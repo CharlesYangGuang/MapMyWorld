1ab0597418f304a7ec9a78953b3d220a7af9154f
charles <charlesyangguang@gmail.com>
2017-03-12 10:38:02 +1000

    screenorientation changed

--- MapMyWorld/MainActivity.cs
+++ MapMyWorld/MainActivity.cs
@@ -1,16 +1,16 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content.PM;
 
 namespace MapMyWorld
 {
-    [Activity(Label = "MapMyWorld", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "MapMyWorld", MainLauncher = true, Icon = "@drawable/icon", ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
         }
