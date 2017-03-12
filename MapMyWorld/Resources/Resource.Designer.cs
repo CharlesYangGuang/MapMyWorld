0ccd48c48fc2d76b2b366a20047b9f3e50e5b03e
charles <charlesyangguang@gmail.com>
2017-03-12 22:04:03 +1000

    second layout and color edited

--- MapMyWorld/Resources/Resource.Designer.cs
+++ MapMyWorld/Resources/Resource.Designer.cs
@@ -97,6 +97,9 @@ namespace MapMyWorld
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
 			
+			// aapt resource value: 0x7f030001
+			public const int page3Layout = 2130903041;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
